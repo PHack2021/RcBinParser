a3addc151074950639c51da447b9e66075f7b97e
fnbdy <22177457+fnbdy@users.noreply.github.com>
2021-06-05 00:53:13 +0800

    Cleanup

--- rc_bin_parser/base_parser.py
+++ rc_bin_parser/base_parser.py
@@ -1,4 +1,28 @@
+import json
+from typing import Dict, List
+
+import requests
+from bs4 import BeautifulSoup
+
+from .rc_bin import RcBin
+
+
 class BaseParser():
 
-    def __init__():
+    def __init__(self, source: dict):
+        self.source = source
+
+    @classmethod
+    def _get_soup(url: str, headers: dict = {}) -> BeautifulSoup:
+        try:
+            r = requests.get(url, headers=headers)
+        except:
+            return False
+
+        if r.status_code != 200:
+            return False
+
+        return BeautifulSoup(r.text, 'lxml')
+
+    def get_rc_bin_data() -> list[Dict]:
         pass
