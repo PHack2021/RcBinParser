9c760171de971c4cbdc669bb3f301f16f439ef93
fnbdy <22177457+fnbdy@users.noreply.github.com>
2021-07-02 15:48:21 +0800

    Template for mapping district codes

--- rc_bin_parser/base_parser.py
+++ rc_bin_parser/base_parser.py
@@ -99,6 +99,12 @@ class BaseParser(ABC):
 
         return organizations
 
+    def _map_district_codes(self) -> None:
+        for rc_bin in self.rc_bins:
+            # 如果 district_code 不存在，從 districts.csv 拿到對應的 district_code 存進去
+            if not rc_bin.get('district_code', ''):
+                pass
+
     def _parse_rc_bins_from_resource(self, resource: Any) -> List[RcBin]:
         rc_bins_raw = self._get_list_from_resource(resource)
 
@@ -135,7 +141,7 @@ class BaseParser(ABC):
             return False
 
         self.rc_bins = self._parse_rc_bins_from_resource(resource)
-
         self.organizations = self._get_unique_organizations()
+        self._get_district_codes()
         # print(self.organizations)
         return self.rc_bins
